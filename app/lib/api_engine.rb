0938aae7e9edb2936a8992bc1dfb8dff6aa9d5c7
Greg Mikeska <digitalintaglio@lavabit.com>
2020-06-17 14:55:52 -0500

    add support for per-endpoint static params

--- app/lib/api_engine.rb
+++ app/lib/api_engine.rb
@@ -16,6 +16,13 @@ class ApiEngine
 
     if(options[:params].present?)
       @params = options[:params]
+    else
+      @params = {}
+    end
+    if(options[:content_type].present?)
+      @content_type = options[:content_type]
+    else
+      @content_type = "plain"
     end
 
     if(options[:query_method].present?)
@@ -30,28 +37,34 @@ class ApiEngine
       args = {}
     end
     args[:endpoint] = endpoint.to_sym
-    info = build_request(args)
-    query_method = info[:query_method]
-    url = info[:url]
+    r_info = build_request(args)
+    query_method = r_info[:query_method]
+    url = r_info[:url]
     puts "this method will #{query_method} #{url}"
+    puts "With query params:"+r_info[:params].keys.collect{|k| "#{k}:#{r_info[:params][k]}"}.join("\n")
   end
 
   def request(**args)
-    info = build_request(args)
+    r_info = build_request(args)
     current_elapsed = DateTime.now.strftime('%s').to_i - @last_query_time
     if(current_elapsed < @query_interval)
       cooldown = @query_interval - current_elapsed
       puts "Sleeping #{cooldown} seconds to accomodate query interval."
       sleep(cooldown)
     end
-    query_method = info[:query_method]
-    url = info[:url]
+    query_method = r_info[:query_method]
+    url = r_info[:url]
+
     puts "#{query_method} #{url}"
-    # puts "this method will #{query_method} #{url}"
     http = Net::HTTP.new(url.host, url.port)
     http.use_ssl = true
     if(query_method == :GET)
       request = Net::HTTP::Get.new(url)
+      if(@params.present?) # Set API-wide header params, such as api-key
+        @params.each_pair do |param_name,param|
+          request[param_name.to_s] = param
+        end
+      end
     end
     if(query_method == :POST)
       request = Net::HTTP::Post.new(url)
@@ -62,18 +75,21 @@ class ApiEngine
     if(query_method == :PUT)
       request = Net::HTTP::Put.new(url)
     end
-    if(@params)
-      @params.each_pair do |param_name,param|
-        request[param_name.to_s] = param
-      end
-    end
 
-    response = http.request(request)
+    if(query_method != :GET)
+      if(@content_type == "json")
+        r_info[:params] = JSON.generate(r_info[:params])
+      elsif(@content_type == "plain")
+        r_info[:params] = URI.encode_www_form(r_info[:params])
+      end
+      response = http.request(request,r_info[:params])
+    else
+      response = http.request(request)
+    end
     @last_query_time = DateTime.now.strftime('%s').to_i
     data = JSON.parse(response.read_body)
-    puts data
-    if(response.code == "200")
-      # puts "success"
+    if(response.code.to_i >= 200 && response.code.to_i < 300)
+      puts "success"
       return data
     else
       puts response.code.to_s+" "+response.message
@@ -100,7 +116,7 @@ class ApiEngine
     @endpoints[e.to_sym][:args].map{|arg| arg.to_sym}
   end
   def query_params_for(e)
-    @endpoints[e.to_sym][:query_params].map{|arg| arg.to_sym}
+    @endpoints[e.to_sym][:query_params].map{|arg| arg.to_sym}.concat(@endpoints[e.to_sym][:set_query_params].keys)
   end
   def prepare_data(data, depth=0,&block)
     if(block.present?)
@@ -131,19 +147,17 @@ class ApiEngine
   private
     def build_request(args)
       if(args[:endpoint])
-        endpoint = @endpoints[args[:endpoint].to_sym]
-        url = url_for(args[:endpoint],args)
+        endpoint_data = @endpoints[args[:endpoint].to_sym]
       end
-
       if(args[:query_method])
-        query_method = args[:query_method].to_sym
-      elsif(endpoint[:query_method])
-        query_method = endpoint[:query_method].to_sym
+        args[:query_method] = args[:query_method].to_s.upcase.to_sym
+      elsif(endpoint_data[:query_method])
+        args[:query_method] = endpoint_data[:query_method].to_s.upcase.to_sym
       else
-        query_method = @query_method.to_sym
+        args[:query_method] = @query_method.to_s.upcase.to_sym
       end
-
-      return {query_method:query_method,url:url}
+      uf = url_for(args[:endpoint],args)
+      return {query_method:args[:query_method],**uf}
     end
 
     def endpoint(name, **args, &block)
@@ -163,7 +177,13 @@ class ApiEngine
           args[:args] << name.to_sym
         end
       end
-      @endpoints[name.to_sym] = {path:args[:path],args:path_args,query_params:args[:query_params], block:block}
+      if(args[:query_params].nil?)
+        args[:query_params] = {}
+      end
+      if(args[:set_query_params].nil?)
+        args[:set_query_params] = {}
+      end
+      @endpoints[name.to_sym] = {path:args[:path],args:path_args,query_params:args[:query_params],query_method:args[:query_method] ,set_query_params:args[:set_query_params], block:block}
     end
 
     def url_for(endpoint, args)
@@ -188,17 +208,26 @@ class ApiEngine
         url.path = url.path + e_path
       end
 
-      if(@endpoints[endpoint.to_sym][:query_params])
-        query_set = []
 
-        query_params_for(endpoint.to_sym).each do |param|
-          if(args[param.to_sym])
-            query_set << [param.to_s,args[param.to_sym].to_s]
-          end
+    if(@endpoints[endpoint.to_sym][:query_params])
+      query_set = {}
+      query_params_for(endpoint.to_sym).each do |param|
+        if(args[param.to_sym])
+          query_set[param.to_sym] = args[param.to_sym]
+        elsif @endpoints[endpoint.to_sym][:set_query_params][param.to_sym]
+          query_set[param.to_sym] =  @endpoints[endpoint.to_sym][:set_query_params][param.to_sym]
         end
-
-        url.query = URI.encode_www_form(query_set)
       end
-      return url
+
+      if(args[:query_method] == :GET)
+        url.query = URI.encode_www_form(query_set)
+        return {url:url,params:{}}
+      else
+        return {url:url,params:query_set}
+      end
+    else
+
+      return {url:url,params:{}}
     end
+  end
 end
