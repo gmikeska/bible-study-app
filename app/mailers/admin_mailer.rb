ecee500d163e78eda053833728376b942c3d1772
Greg Mikeska <digitalintaglio@lavabit.com>
2020-07-13 06:01:50 -0500

    update sender email in email message UI

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -1,10 +1,10 @@
 class AdminMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
-    mail(to: @user.email, subject: "Welcome, #{@user.name}! You're an Admin!", from: 'noreply@church-sunday-school.herokuapp.com')
+    mail(to: @user.email, subject: "Welcome, #{@user.name}! You're an Admin!", from:"Memorial United Methodist Church <website@memorialumcaustin.com>")
   end
   def admin_logs_in
     @user = params[:user]
-    mail(to: "gmikeska07@gmail.com", subject: "An admin just logged in.", from: 'noreply@church-sunday-school.herokuapp.com')
+    mail(to: "gmikeska07@gmail.com", subject: "An admin just logged in.", from:"Memorial United Methodist Church <website@memorialumcaustin.com>")
   end
 end
